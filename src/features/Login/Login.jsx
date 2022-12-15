76a3f31360dc5a671235c41391c59961ea16bb07
luongsybao <luongsybao2505@gmail.com>
2022-12-15 19:08:53 +0700

    login layout conti

--- src/features/Login/Login.jsx
+++ src/features/Login/Login.jsx
@@ -1,7 +1,81 @@
+import { Button, Checkbox, Form, Input } from "antd";
 import React from "react";
+import { useDispatch } from "react-redux";
 
 const Login = () => {
-  return <div>Login</div>;
+  const dispatch = useDispatch();
+  const handleLogin = (value) => {
+    console.log(value);
+  };
+  return (
+    <div className="container ">
+      <div className="flex justify-center mt-10">
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={handleLogin}
+          // onFinishFailed={}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="taiKhoan"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="matKhau"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
