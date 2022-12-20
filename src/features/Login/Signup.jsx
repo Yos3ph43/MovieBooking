f24d665d00a3200ea7968a45b2308aaa562aed8b
sybao <luongsybao2505@gmail.com>
2022-12-20 16:11:16 +0700

    signup conti

--- src/features/Login/Signup.jsx
+++ src/features/Login/Signup.jsx
@@ -1,7 +1,116 @@
+import { Button, Input, Form } from "antd";
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const handleAddMember = (value) => {
+    console.log(value);
+  };
+  return (
+    <div className="container ">
+      <div className="flex justify-center mt-10">
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={handleAddMember}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="taiKhoan"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="matKhau"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: "Please input your email!",
+              },
+            ]}
+          >
+            <Input placeholder="...@gmail.com" />
+          </Form.Item>
+          <Form.Item
+            label="Phone"
+            name="soDt"
+            rules={[
+              {
+                required: true,
+                message: "Please input your phone number!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          {/* group number hidden  */}
+          <Form.Item
+            hidden="false"
+            label="Group number"
+            name="maNhom"
+            initialValue="GP10"
+          >
+            <Input disabled={true} />
+          </Form.Item>
+          <Form.Item
+            label="Name"
+            name="hoTen"
+            rules={[
+              {
+                required: true,
+                message: "Please input your name!",
+              },
+            ]}
+          >
+            <Input placeholder="Nguyễn Văn A" />
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+        <div>
+          <p>duc3636</p>
+          <p>nguyen1</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
